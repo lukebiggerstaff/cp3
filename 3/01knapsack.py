616642cfebd4107ce6d069affbe35ebef9ffc51a
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2020-02-09 17:20:10 -0800

    added complete search 0/1 knapsack algorithm and top down dynamic programming 0/1 knapsack algorithm

--- 3/01knapsack.py
+++ 3/01knapsack.py
@@ -1,7 +1,50 @@
-def knap(s, v, w):
+def csknap(s, v, w):
     """
-    0/1 knapsack implementation, s is total capacity
-    v is value of object and w is weight of object
+    0/1 knapsack implemented as a recursive complete search algorithm
+    """
+
+    def val(item, remw):
+        # return 0 if there is no weight left
+        # or you have iterated past the end of items
+        if remw <= 0 or item >= len(v):
+            return 0
+        # if item weight fits into remaining weight
+        # see if the overall value is better w or w/out item
+        if w[item] <= remw:
+            return max(v[item] + val(item + 1, remw - w[item]), val(item + 1, remw))
+        return val(item + 1, remw)
+
+    return val(0, s)
+
+
+def tdknap(s, v, w):
+    """
+    0/1 knapsack implemented as a recursive top-down
+    dynamic programming algorithm 
+    """
+    memo = dict()
+
+    def val(item, remw):
+        if remw <= 0 or item >= len(v):
+            return 0
+        state = (item, remw)
+        if state in memo:
+            return memo[state]
+        if w[item] <= remw:
+            memo[state] = max(
+                v[item] + val(item + 1, remw - w[item]), val(item + 1, remw)
+            )
+            return memo[state]
+        return val(item + 1, remw)
+
+    return val(0, s)
+
+
+def buknap(s, v, w):
+    """
+    0/1 knapsack implementation via bottom-up dp
+    s is total capacity, v is value of object 
+    and w is weight of object
     """
     items = len(v)
     weights = s
@@ -16,8 +59,16 @@ def knap(s, v, w):
 
 
 if __name__ == "__main__":
-    v = [100, 70, 50, 10]
-    w = [10, 4, 6, 12]
+    v = [100, 80, 90, 10, 70, 50]
+    w = [10, 4, 8, 12, 4, 6]
     s = 12
-    res = knap(s, v, w)
-    assert res == 120
+
+    res = csknap(s, v, w)
+    assert res == 170
+
+    res2 = tdknap(s, v, w)
+    assert res2 == 170
+
+    res3 = buknap(s, v, w)
+    assert res3 == 170
+
