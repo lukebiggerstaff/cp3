fc7b79eb88b445de6fd92492fd3f4a67a5dfa0a8
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2020-02-12 14:36:03 -0800

    added complete search algorithm for longest increasing subsequence and refactored existing dp algorithm to calculate max value while pre-processing the subsequence value array

--- 3/lis.py
+++ 3/lis.py
@@ -1,3 +1,24 @@
+def cslis(l):
+    """
+    complete search algorithm for finding
+    the longest increasing subsequence
+    """
+
+    def pset(l):
+        n = len(l)
+        for i in range(1, (1 << n)):
+            yield [l[j] for j in range(n) if (i & (1 << j))]
+
+    max_value = 0
+    for ss in pset(l):
+        for i in range(1, len(ss)):
+            if ss[i - 1] > ss[i]:
+                break
+            elif i >= len(ss) - 1:
+                max_value = max(max_value, len(ss) - 1)
+    return max_value
+
+
 def lis(l):
     """
     the longest increasing subsequence is the longest stretch
@@ -8,6 +29,7 @@ def lis(l):
     """
     n = len(l)
     # initialize subs list to hold subsequence values
+    max_value = 0
     subs = [1 for _ in range(n)]
     for i in range(1, n):
         for j in range(i):
@@ -15,16 +37,16 @@ def lis(l):
                 # subs[i] is either the same as before
                 # or whatever the value in subs[j] + 1
                 subs[i] = max(subs[j] + 1, subs[i])
-    max_value = 0
-    # iterate over subs to find highest value for
-    # the longest increasing subsequence
-    for val in subs:
-        if val > max_value:
-            max_value = val
+                # update max_value while iterating
+                max_value = max(subs[i], max_value)
     return max_value
 
 
 if __name__ == "__main__":
     l = [-7, 10, 9, 2, 3, 8, 8, 1]
+
     res = lis(l)
     assert res == 4
+
+    res2 = cslis(l)
+    assert res2 == 4
