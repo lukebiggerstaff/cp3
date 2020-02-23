2c323e5090a4e426ca8c6df96418833987148e32
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2020-02-22 18:21:41 -0800

    removed unnecessary visited data structure, the color of the nodes serves as the visited flag.

--- 4/bipartite.py
+++ 4/bipartite.py
@@ -3,21 +3,27 @@ from collections import deque
 
 def is_bipartite(al):
     """
-    algorithm to determine if a graph is bipartite.
+    This algorithm determines if a graph is bipartite by traversing 
+    breadth-first and coloring each node one of two colors, current node
+    is one color and all its neighbors are another. If a node finds 
+    an already colored/visited node and it is not the opposite color
+    then we know that the graph is not bipartite.
     """
-    visited = [0 for _ in range(len(al))]
     color = [(1 << 20) for _ in range(len(al))]
 
+    # color first point 0
     color[0] = 0
     queue = deque([0])
     while queue:
         i = queue.popleft()
         i_color = color[i]
-        visited[i] = 1
         for j in al[i]:
+            # color node if it is not already colored
             if color[j] == (1 << 20):
                 queue.append(j)
                 color[j] = 1 - i_color
+            # if color matches current node color
+            # than graph is not bipartite.
             if color[j] == i_color:
                 return False
     return True
