5d0a633a994ea35728fd81ed1f9047cbb615e136
Luke Biggerstaff <luke.biggerstaff@gmail.com>
2020-03-02 13:04:28 -0800

    finished algorithm for edmondkarp to find maximum flow on a directed weighted graph

--- 4/edmondkarp.py
+++ 4/edmondkarp.py
@@ -1,11 +1,21 @@
+from collections import deque
 from queue import Queue
 
 
 def edmondkarp(am, s, t):
     n = len(am)
     max_flow = 0
+    res = [[0 for _ in range(n)] for _ in range(n)]
 
-    def find_path():
+    def find_path(parent, i, path=None):
+        if path is None:
+            path = deque()
+        if i == s:
+            return path
+        path.appendleft((parent[i], i))
+        return find_path(parent, parent[i], path)
+
+    def bfs():
         visited = [0 for _ in range(n)]
         parent = [v for v in range(n)]
         queue = Queue(maxsize=0)
@@ -14,17 +24,25 @@ def edmondkarp(am, s, t):
             u = queue.get()
             visited[u] = 1
             for v, cap in enumerate(am[u]):
-                if visited[v] == 0 and cap > 0:
+                if visited[v] == 0 and (cap - res[u][v]) > 0:
                     parent[v] = u
                     if v == t:
-                        return parent
+                        return find_path(parent, v)
                     queue.put(v)
         return []
 
-    path = find_path()
+    path = bfs()
+    while len(path) > 0:
+        flow = min(am[u][v] - res[u][v] for u, v in path)
+        max_flow += flow
+        for u, v in path:
+            res[u][v] += flow
+            res[v][u] -= flow
+        path = bfs()
+    return max_flow
 
 
 if __name__ == "__main__":
     am = [[0, 0, 70, 30], [0, 0, 0, 0], [0, 25, 0, 5], [0, 70, 0, 0]]
     res = edmondkarp(am, 0, 1)
-    print(res)
+    assert res == 60
